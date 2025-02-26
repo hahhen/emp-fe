12a2a2d2d5adefd9bda8aeab46ccd0f9a1eda376
Hahhen <arthurmp2007@hotmail.com>
2025-02-26 19:23:15 -0300

    Revert "Implementing stripe"
    This reverts commit 62fa8906e7d1921505d0f13bda33b51d7b6f6292.

--- src/app/cart/layout.jsx
+++ src/app/cart/layout.jsx
@@ -1,13 +1,7 @@
-import ElementsProvider from "@/lib/elementsprovider"
-
 export const metadata = {
     title: "EMP Store | Cart",
 }
 
-export default function CartLayout({ children }) {
-    return (
-        <ElementsProvider>
-            {children}
-        </ElementsProvider>
-    )
+export default function CartLayout({children}) {
+    return children;
 }
